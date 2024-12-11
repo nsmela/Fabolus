eaf546f09e3e93b81167bacaa758cba29da858f2
NathanSmela <nsmela@hotmail.com>
2024-12-11 14:58:30 -0800

    more work, think the circle's origin needs to be calculated.

--- Fabolus.Core/AirChannel/AngledChannelCurve.cs
+++ Fabolus.Core/AirChannel/AngledChannelCurve.cs
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 
 namespace Fabolus.Core.AirChannel;
 public static class AngledChannelCurve {
-    private const int SEGMENTS = 16;
+    private const int SEGMENTS = 32;
 
     public static List<Vector3d> Curve(Vector3d origin, Vector3d direction, double radius, double offset = 1.0) {
         var dir = direction.Normalized;
         var angle = 360.0 - Vector3d.AngleD(Vector3d.AxisZ, dir);
 
+        //or use vectors to make the arc
         var arc = new Arc2d(new Vector2d(0, radius + offset), radius + offset, angle, 360.0);
 
         var points = new List<Vector3d>();
         points.Add(origin);
-        double span = (1 / (double)SEGMENTS);
-        for (int i = 0; i <= (double)SEGMENTS; i++) {
+        double span = (arc.ArcLength / (double)SEGMENTS);
+        for (int i = 0; i <= SEGMENTS; i++) {
             var point = arc.SampleT(span * i);
             points.Add(new Vector3d(
                 origin.x + (dir.x * point.x),
