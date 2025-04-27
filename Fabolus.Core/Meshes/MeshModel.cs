26814b98f81333a5e03df98a274de715d9d93c5f
NathanSmela <nsmela@hotmail.com>
2025-04-27 12:47:04 -0700

    progress

---
+++ Fabolus.Core/Meshes/MeshModel.cs
@@ -0,0 +1,82 @@
+﻿using g3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MR.DotNet;
+
+namespace Fabolus.Core.Meshes;
+public class MeshModel {
+    private DMesh3 Mesh { get; set; } = new DMesh3();
+
+    public bool IsEmpty() => Mesh is null || Mesh.TriangleCount == 0;
+
+    public IEnumerable<(double, double, double)> TriangleVectors() => Mesh.Vertices().Select(v => (v.x, v.y, v.z));
+    public IEnumerable<(double, double, double)> NormalVectors() {
+        for (int i = 0; i < mesh.VertexCount; i++) {
+            yield return mesh.GetVertexNormal(i).ToVector3());
+        }
+    }
+
+    public IEnumerable<(int, int, int)> TriangleIndices() => Mesh.TriangleIndices().Select(t => (t.a, t.b, t.c));
+
+    public IList<int> TriangleIndexes() {
+        var triangles = new List<int>();
+        foreach (var tri in mesh.Triangles()) {
+            triangles.Add(tri.a);
+            triangles.Add(tri.b);
+            triangles.Add(tri.c);
+        }
+        return triangles;
+    }
+
+    public double Volume {
+        get {
+            if (Mesh is null || Mesh.VertexCount == 0) { return 0.0; }
+
+            var volumeAndArea = MeshMeasurements.VolumeArea(Mesh, Mesh.TriangleIndices(), Mesh.GetVertex);
+            return volumeAndArea.x / 1000;
+        }
+    }
+
+    // Constructors
+
+    public MeshModel() { }
+
+    public MeshModel(DMesh3 mesh) {
+        Mesh = mesh;
+    }
+    public MeshModel(Mesh mesh) {
+        Mesh = mesh.ToDMesh();
+    }
+
+    // Conversion methods
+
+    public static DMesh3 ToDMesh(this Mesh mesh) {
+        DMesh3 result = new();
+
+        foreach (var p in mesh.Points) {
+            result.AppendVertex(new Vector3d(p.X, p.Y, p.Z));
+        }
+        foreach (var t in mesh.Triangulation) {
+            result.AppendTriangle(t.v0.Id, t.v1.Id, t.v2.Id);
+        }
+
+        return result
+    }
+
+    public static Mesh ToMesh(this DMesh3 mesh) {
+        List<MR.DotNet.Vector3f> verts = mesh.Vertices().Select(v => new MR.DotNet.Vector3f((float)v.x, (float)v.y, (float)v.z)).ToList();
+        List<ThreeVertIds> tris = mesh.Triangles().Select(t => new ThreeVertIds(t.a, t.b, t.c)).ToList();
+        return Mesh.FromTriangles(verts, tris);
+    }
+    
+    // Operators
+
+    public static implicit operator DMesh3(MeshModel model) => model.Mesh;
+    public static explicit operator MeshModel(DMesh3 mesh) => new(mesh);
+
+    public static implicit operator Mesh(MeshModel model) => model.Mesh.ToMesh();
+    public static explicit operator MeshModel(Mesh model) => new(mesh);
+}
---
+++ Fabolus.Wpf/Common/Extensions/MeshModelExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Fabolus.Core.Meshes;
+using g3;
+using HelixToolkit.Wpf.SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Fabolus.Wpf.Common.Extensions;
+public static class MeshModelExtensions {
+    public static MeshGeometry3D ToGeometry(this MeshModel mesh) {
+        if (mesh.IsEmpty()) { return new MeshGeometry3D(); }
+
+        var geometry = new MeshBuilder(true, false, false);
+
+        geometry.Append(
+            mesh.TriangleVectors().Select(v => new Point3D(v.Item1, v.Item2, v.Item3)).ToArray(), //3D vert positions
+            mesh.TriangleIndices(), //index of each triangle's vertex
+            NormalsList(mesh), //normals
+            null); // texture coordinates
+
+        return geometry.ToMeshGeometry3D();
+    }
+
+    private static List<int> TrianglesList((int, int, int) triangles) {
+        var triangles = new List<int>();
+        foreach (var tri in triangles) {
+            triangles.Add(tri);
+            triangles.Add(tri.b);
+            triangles.Add(tri.c);
+        }
+        return triangles;
+    }
+}
