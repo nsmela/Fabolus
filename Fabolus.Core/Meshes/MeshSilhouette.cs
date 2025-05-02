c504e3c4b0fdb4f390eeebaa870fe03fef51c12a
NathanSmela <nsmela@hotmail.com>
2025-05-01 21:00:32 -0700

    progress

--- Fabolus.Core/Meshes/MeshSilhouette.cs
+++ Fabolus.Core/Meshes/MeshSilhouette.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,30 +13,48 @@ public static class MeshSilhouette {
     //ref: https://www.angusj.com/clipper2/Docs/Overview.htm
 
     public static Vector2d[] MeshToSilhouette(DMesh3 mesh) {
-        Paths64 paths = new();
+        PathsD paths = new();
 
         foreach(var t in mesh.Triangles()) {
-            Path64 trianglePath = new Path64 {
-                mesh.GetVertex(t.a).ToPoint64(),
-                mesh.GetVertex(t.b).ToPoint64(),
-                mesh.GetVertex(t.c).ToPoint64(),
+            PathD trianglePath = new PathD {
+                mesh.GetVertex(t.a).ToPointD(),
+                mesh.GetVertex(t.b).ToPointD(),
+                mesh.GetVertex(t.c).ToPointD(),
             };
 
             paths.Add(trianglePath);
         }
 
-        Paths64 result = Clipper.Union(paths, FillRule.NonZero);
+        var testPaths = new PathsD();
+        var subject = new PathD {
+            new PointD{x = -50.0, y = -50.0},
+            new PointD{x = -50.0, y =  50.0},
+            new PointD{x =  50.0, y =  50.0},
+            new PointD{x =  50.0, y = -50.0},
+        };
+
+        var clip = new PathD {
+            new PointD{x = -100.0, y = -30.0},
+            new PointD{x = -100.0, y =  30.0},
+            new PointD{x =  100.0, y =  30.0},
+            new PointD{x =  100.0, y = -30.0},
+        };
+
+        var result = Clipper.Union(new PathsD { paths[0] }, new PathsD { paths[1] }, FillRule.NonZero, 1);
+        foreach(var path in paths) {
+            result = Clipper.Union(result, new PathsD { path }, FillRule.NonZero, 1);
+        }
         List<Vector2d> contour = [];
 
         foreach(var polyline in result) {
             foreach (var p in polyline) {
-                contour.Add(new Vector2d(p.X, p.Y));
+                contour.Add(new Vector2d(p.x, p.y));
             }
         }
 
         return contour.ToArray();
     }
 
-    private static Point64 ToPoint64(this Vector3d vector) => new Point64(vector.x, vector.y);
+    private static PointD ToPointD(this Vector3d vector) => new PointD(vector.x, vector.y);
 
 }
