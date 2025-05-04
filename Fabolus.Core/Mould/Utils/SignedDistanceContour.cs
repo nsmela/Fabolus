0b7d66eae54c0826baee04a8f01b5a0ba98ebf3d
NathanSmela <nsmela@hotmail.com>
2025-05-03 19:38:16 -0700

    progress

--- Fabolus.Core/Mould/Utils/SignedDistanceContour.cs
+++ Fabolus.Core/Mould/Utils/SignedDistanceContour.cs
@@ -1,10 +1,5 @@
 ﻿using g3;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fabolus.Core.Mould.Utils;
 
