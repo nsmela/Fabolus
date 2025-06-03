2cce6d7886acd59f69cc07a9f2fba6d05bee5099
NathanSmela <nsmela@hotmail.com>
2025-06-03 15:00:49 -0700

    initial testing for double offsetting for smoothing

--- Fabolus.Core/Smoothing/MarchingCubesSmoothing.cs
+++ Fabolus.Core/Smoothing/MarchingCubesSmoothing.cs
@@ -1,5 +1,6 @@
 ﻿using Fabolus.Core.BolusModel;
 using g3;
+using gs;
 
 namespace Fabolus.Core.Smoothing;
 
@@ -13,8 +14,10 @@ public class MarchingCubesSmoothing {
         var iterations = settings.Iterations;
         var cells = settings.Cells;
 
+        //offset mesh larger and then shrink a bit to lose details
+        var mesh = OffsetBolus(bolus, 64, 0.5, 0.5);
+
         //Use the Remesher class to do a basic remeshing
-        DMesh3 mesh = new DMesh3(bolus.Mesh);
         Remesher r = new Remesher(mesh);
         r.PreventNormalFlips = true;
         r.SetTargetEdgeLength(edgeLength);
@@ -45,5 +48,42 @@ public class MarchingCubesSmoothing {
         newBolus.CopyOffsets(bolus);
 
         return newBolus;
+    }
+
+    private static DMesh3 OffsetBolus(Bolus bolus, int resolution, double offset1, double offset2) {
+        BoundedImplicitFunction3d meshImplicit = meshToImplicitF(bolus.Mesh, resolution, offset1);
+        var first =  generatMeshF(new ImplicitOffset3d() { A = meshImplicit, Offset = offset1 }, resolution);
+        BoundedImplicitFunction3d secondMeshImplicit = meshToImplicitF(first, resolution, offset2);
+        return generatMeshF(new ImplicitOffset3d() { A = secondMeshImplicit, Offset = offset2 }, resolution);
+    }
+
+    // meshToImplicitF() generates a narrow-band distance-field and
+    // returns it as an implicit surface, that can be combined with other implicits                       
+    private static Func<DMesh3, int, double, BoundedImplicitFunction3d> meshToImplicitF = (meshIn, numcells, max_offset) => {
+        double meshCellsize = meshIn.CachedBounds.MaxDim / numcells;
+        MeshSignedDistanceGrid levelSet = new MeshSignedDistanceGrid(meshIn, meshCellsize);
+        levelSet.ExactBandWidth = (int)(max_offset / meshCellsize) + 1;
+        levelSet.Compute();
+        return new DenseGridTrilinearImplicit(levelSet.Grid, levelSet.GridOrigin, levelSet.CellSize);
+    };
+
+    // generateMeshF() meshes the input implicit function at
+    // the given cell resolution, and writes out the resulting mesh    
+    private static DMesh3 generatMeshF(BoundedImplicitFunction3d root, int numcells) {
+        MarchingCubes c = new MarchingCubes();
+        c.Implicit = root;
+        c.RootMode = MarchingCubes.RootfindingModes.LerpSteps;      // cube-edge convergence method
+        c.RootModeSteps = 5;                                        // number of iterations
+        c.Bounds = root.Bounds();
+        c.CubeSize = c.Bounds.MaxDim / numcells;
+        c.Bounds.Expand(3 * c.CubeSize);                            // leave a buffer of cells
+        c.Generate();
+        MeshNormals.QuickCompute(c.Mesh);                           // generate normals
+
+        // cleanup
+        MeshAutoRepair repair = new(c.Mesh);
+        repair.Apply();
+
+        return repair.Mesh;
     }
 }
