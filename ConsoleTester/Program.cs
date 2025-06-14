a94dcf8339638f9d7e27df85ee5cf364dda66a8d
NathanSmela <nsmela@hotmail.com>
2025-06-14 07:48:21 -0700

    managed to make a dll file, but it has no external functions

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,11 @@
+﻿using Fabolus.Core.Meshes.PoissonRecon;
+
+namespace ConsoleTester;
+
+internal class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        double value = PoissonSmoothingGenerator.Test();
+        Console.ReadLine();
+    }
+}
---
+++ Fabolus.Core/Meshes/PoissonRecon/PoissonSmoothingGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabolus.Core.Meshes.PoissonRecon;
+
+/// <summary>
+/// Wrapper class for Poison Reconstruction dll
+/// </summary>
+public class PoissonSmoothingGenerator {
+
+    public static double Test() => PoissonReconDLLAdapter.PeakMemoryUsageMB();
+
+    private static class PoissonReconDLLAdapter {
+        // ref: https://www.youtube.com/watch?v=fXO7rU2-qu8
+        // https://learn.microsoft.com/en-us/dotnet/standard/native-interop/pinvoke
+        // https://www.vishalon.net/blog/cpp-dll-get-exported-symbols-list-on-windows
+        // dll is PoissonRecon version 9.011
+        // https://github.com/mkazhdan/PoissonRecon/tree/16375a78928bd67b4cad1a410131f4b5f0afabde
+
+        [DllImport("PoissonRecon.dll", EntryPoint = "PeakMemoryUsageMB")]
+        public static extern double PeakMemoryUsageMB();
+    }
+}
