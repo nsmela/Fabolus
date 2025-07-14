60d23541d8aab18a35df32c7c83f72630f9de806
NathanSmela <nsmela@hotmail.com>
2025-07-14 08:00:29 -0700

    adjusted default mould settings

--- Fabolus.Core/Mould/Builders/MouldGenerator.cs
+++ Fabolus.Core/Mould/Builders/MouldGenerator.cs
@@ -10,9 +10,9 @@ public abstract record MouldGenerator {
     protected DMesh3 BolusReference { get; set; } //mesh to invert while entirely within
     public int CalculationResolution { get; protected set; } = 32; //how accurate the implicit meshs are (higher is better, but slower)
     public double ContourResolution { get; protected set; } = 1.0; //xy contour detection grid size (lower is better, but slower)
-    public double OffsetXY { get; protected set; } = 4.0;
-    public double OffsetTop { get; protected set; } = 3.0;
-    public double OffsetBottom { get; protected set; } = 3.0;
+    public double OffsetXY { get; protected set; } = 3.0;
+    public double OffsetTop { get; protected set; } = 2.0;
+    public double OffsetBottom { get; protected set; } = 2.0;
     protected DMesh3[] ToolMeshes { get; set; } = []; // mesh to boolean subtract from the mold
 
     public abstract Result<MeshModel> Build();
