44a1bfa9a73426e0c898f3723e2b92e6627f7c1c
NathanSmela <nsmela@hotmail.com>
2025-07-22 09:27:09 -0700

    geodisc pathing working!

--- Fabolus.Core/Meshes/PartingTools/GeodiscPathing.cs
+++ Fabolus.Core/Meshes/PartingTools/GeodiscPathing.cs
@@ -12,104 +12,118 @@ public static partial class PartingTools {
 
     private record struct Wedge {
         public int a, b, c;
-        public int[] triangles; // ordered list of triangles in wedge
-        public int[] edges; // edge opposite centre vertex in the matching triangle index
+        public int[] boundry_points; // edge opposite centre vertex in the matching triangle index
         public double min_angle;
 
         public Wedge(DMesh3 mesh, int v0, int v1, int v2) {
             this.a = v0;
             this.b = v1;
             this.c = v2;
-            triangles = [];
-            edges = [];
+
+            boundry_points = [a,b,c]; // default value incase of errors while constructing
 
             // determine the starting point for calculations
             int eId = mesh.FindEdge(v0, v1);
+            if(eId == DMesh3.InvalidID) { return; }
             Index4i edge = mesh.GetEdge(eId); // starting edge
 
             int tId = edge.c;
             Index3i triangle = mesh.GetTriangle(tId); // starting triangle
-            int vId = GetOpposingTriangleV(mesh, tId, v0, v1); // vId, v1 and tId is used to find the next triangle
+            int vId = GetOpposingTriangleV(mesh, tId, v0, v1); // vId, v1 and tId is used to find the next vertex
 
-            // collect the triangles surrounding the vertex, ordered. Direction doesn't matter
-            List<int> tris = [];
-            List<int> outer_edges = []; // edges that are on the outer boundry of the wedge
-            outer_edges.Add( mesh.GetTriEdge(tId, v0)); // edge id of the triangle that is opposite the centre vertex
-            while (!tris.Contains(tId)) {
-                tris.Add(tId);
+            // collect the boundry points
+            List<int> outer_points = [v0]; // points that are on the outer boundry of the wedge, starting at v0
+
+            while (vId != v0) {
+                outer_points.Add(vId);
+
                 tId = GetOpposingTriangle(mesh, v1, vId, tId); // get the other triangle from the shared edge
                 vId = GetOpposingTriangleV(mesh, tId, v1, vId);
-                outer_edges.Add(mesh.GetTriEdge(tId, v0));
             }
 
             // calculate angles for each outside boundry
-            var tris_list = tris.ToList();
 
             double total_angle = 0.0;
-            List<int> list = [v0];
-            // int tId is reused from earlier
-            // int triangle is used from before
-            for (int i = 0; i < tris_list.Count; i++) {
-                tId = tris_list[i];
-                triangle = mesh.GetTriangle(tId);
-                if (triangle.array.Any(v => v == v2)) { break; }
-
+            int end_index = outer_points.IndexOf(v2) + 1; // end at the  wedge end, with offset zero indexing
+            for (int i = 1; i < end_index; i++) {
                 // need to ensure this is correct
                 // TODO: convert to rads, leave in degrees for now for easier troubleshooting
-                var angle = GetTriangleAngle(mesh, tId, v1);
+                var angle = GetAngleBetweenVectors(mesh, outer_points[i - 1], v1, outer_points[i]);
                 total_angle += angle;
-                list.Add(tId);
 
+                
             }
 
             // determine which set of triangles are valid: the ones we scanned or the ones remaining
             if (total_angle < 180.0) {
                 // this is a valid wedge, store the indexes
-                triangles = list.ToArray();
-                edges = outer_edges.Take(list.Count).ToArray();
+                boundry_points = outer_points.Take(end_index).ToArray();
                 min_angle = total_angle;
             } else {
                 // this is not a valid wedge, store the opposite side
-                triangles = tris_list.Skip(list.Count).ToArray();
-                edges = outer_edges.Skip(list.Count).ToArray();
+                outer_points.Add(v0);
+                boundry_points = outer_points.Skip(end_index - 1).Reverse().ToArray();
                 min_angle = 360.0 - total_angle;
+            }
+        }
+
+        public double GetOuterBoundryAngle(DMesh3 mesh, int vId) {
+            int v0 = boundry_points[vId - 1];
+            int v1 = boundry_points[vId];
+            int v2 = boundry_points[vId + 1];
+
+            Vector3d v1_v0 = mesh.GetVertex(v1) - mesh.GetVertex(v0);
+            Vector3d v1_v2 = mesh.GetVertex(v1) - mesh.GetVertex(v2);
+
+            // return value
+            return v1_v0.AngleD(v1_v2);
+        }
+
+        public int[] FlipoutPath(DMesh3 mesh) {
+            if (boundry_points.Length < 3) {
+                return [a, b, c];
+            } // insufficient points to form a path
+            List<int> path = [boundry_points[0]]; // use first point
+            for (int i = 1; i < boundry_points.Length - 1; i++) {
+                // if the angle is less than 180, we can skip the current point by "flipping" the edge
+                double angle = GetOuterBoundryAngle(mesh, i);
+                if (angle > 180.0) {
+                    path.Add(boundry_points[i]); // do not flip, add the current point to the path
+                } else {
+                    path.Add(boundry_points[i + 1]); // flip the edge, add the next point to the path
+                }
+            }
+
+            // compare path distances
+            double original_length = GetPathDistance(mesh, [a, b, c]);
+            double fliped_path = GetPathDistance(mesh, path.ToArray());
+
+            if (original_length < fliped_path) { return [b]; } // no change needed
+
+            Remesh(mesh, path.ToArray()); // remesh the path
+            path.RemoveAt(path.Count - 1); // remove the last point
+            path.RemoveAt(0); // remove the first point
+            return path.ToArray(); // return the path without the first and last points
+        }
+
+        // split edges and triangles to make the new path valid along the mesh
+        // TODO: this is not implemented yet, need to figure out how to split the triangles and edges
+        private void Remesh(DMesh3 mesh, int[] path) {
+            return;
+            // assume v0, v1 and v2 are valid points and along existing edges
+            // path starts with v0, ends with v2
+            for (int i = 1; i < path.Length; i++) {
+                // edge exists between this point and previous point?
+                int eId = mesh.FindEdge(path[i - 1], path[i]);
+                if (eId != DMesh3.InvalidID) { continue; } // edge exists, no work needed
+
+                // edge does not exist, we need to create it
+                // there is an edge between v1 and path[i] and an edge for v1 and path[i - 1]
+                // we need the two triangle ids
+                // split their shared edge along where the new edge should show
             }
 
         }
-    }
-
-    private static double GetOuterBoundryAngle(DMesh3 mesh, int t0, int t1, int v0) {
-        int a, b, c;
-        Index3i tri0 = mesh.GetTriangle(t0);
-        Index3i tri1 = mesh.GetTriangle(t1);
-
-        if (tri0.a != v0 && tri1.array.Any(v => v == tri0.a)) {
-            b = tri0.a;
-            a = tri0.a != v0 ? tri0.a : tri0.c;
-        }
-        else if (tri0.b != v0 && tri1.array.Any(v => v == tri0.b)) {
-            b = tri0.b;
-            a = tri0.b != v0 ? tri0.b : tri0.c;
-        } else if (tri0.c != v0 && tri1.array.Any(v => v == tri1.c)) {
-            b = tri0.c;
-            a = tri0.c != v0 ? tri0.c : tri0.a;
-        } else {
-            throw new Exception("Invalid triangle configuration for outer boundry angle calculation.");
-        }
-        c = tri1.array.Where(v => v != v0 && v != a).FirstOrDefault();
-
-        return GetAngleBetweenVectors(mesh, a, b, c);
-
-    }
-
-    private static double GetTriangleAngle(DMesh3 mesh, int tId, int origin_vertex) {
-        Index3i triangle = mesh.GetTriangle(tId);
-        Vector3d v0 = mesh.GetVertex(triangle.a);
-        Vector3d v1 = mesh.GetVertex(triangle.b);
-        Vector3d v2 = mesh.GetVertex(triangle.c);
-        Vector3d v1_v0 = v1 - v0;
-        Vector3d v1_v2 = v1 - v2;
-        return v1_v0.AngleD(v1_v2);
     }
 
     private static int GetOpposingTriangle(DMesh3 mesh, int a, int b, int triangle_id) {
@@ -131,44 +145,53 @@ public static partial class PartingTools {
         Vector3d vec0 = mesh.GetVertex(v0);
         Vector3d vec1 = mesh.GetVertex(v1);
         Vector3d vec2 = mesh.GetVertex(v2);
-        Vector3d v1_v0 = vec1 - vec0;
-        Vector3d v1_v2 = vec1 - vec2;
+        Vector3d v1_v0 = (vec1 - vec0).Normalized;
+        Vector3d v1_v2 = (vec1 - vec2).Normalized;
         return v1_v0.AngleD(v1_v2);
     }
 
+    private static double GetPathDistance(DMesh3 mesh, params int[] path) {
+        double total = 0.0;
+        if (path.Length < 2) { return total; } // insufficient path length
+        for (int i = 1; i < path.Length; i++) {
+            total += mesh.GetVertex(path[i - 1]).Distance(mesh.GetVertex(path[i]));
+        }
+
+        return total;
+    }
+
     public sealed class GeodiscPathing {
-        private double[] _edge_lengths;
         private int[] _original_path;
         private List<int> _path;
+        private DMesh3 _mesh;
         private bool _dirty = true; // flag to determine if FinalCompute needs to be recomputed
 
-        public GeodiscPathing(DMesh3 mesh, int[] path) {
-            _edge_lengths = new double[path.Length];// preset collection size for optimization
+        public GeodiscPathing(DMesh3 mesh, IEnumerable<int> path) {
+            int count = path.Count();
+            _mesh = mesh;
 
-            // calculate edge lengths
-            Vector3d v0, v1;
-            for (int i = 0; i < path.Length - 1; i++) {
-                v0 = mesh.GetVertex(path[i]);
-                v1 = mesh.GetVertex(path[i + 1]);
-                _edge_lengths[i] = v0.Distance(v1);
-            }
-
-            _original_path = path;
+            _original_path = path.ToArray();
             _path = _original_path.ToList();
-
-            Wedge wedge = new(mesh, path[0], path[1], path[2]);
         }
 
         public void Compute() {
             _dirty = true; // mark as dirty to recompute
-            // create a wedge for each abc vertex in path
 
-            // for each wedge:
-            // for each wedge triangle, check outer boundry angle
-            // if lower than pi, continue. already optimal
-            // if higher than pi, flip the edge by adding the vert for the flipped triangle edge (mesh.GetEdgeOpposingV?)
+            List<int> new_path = [_path[0]];
+            int v0, v1, v2;
+            Wedge wedge;
+            for (int i = 1; i < _path.Count - 1; i++) {
+                // create a wedge for each abc vertex in path
+                v0 = _path[i - 1];
+                v1 = _path[i];
+                v2 = _path[i + 1];
 
-            // when done, add v2 to the path (unless already added by the last triangle flip)
+                wedge = new Wedge(_mesh, v0, v1, v2);
+                new_path.AddRange(wedge.FlipoutPath(_mesh)); // add the flipped path to the new path
+
+            }
+            _path = new_path;
+            
         }
 
         // splits the triangles on the mesh, setting the path and mesh for retreival
@@ -178,25 +201,17 @@ public static partial class PartingTools {
 
         // converts the path to a list of Vector3d points
         // also splits triangles on the mesh to map the 
-        public Vector3d[] GetPath() {
-            FinalCompute();
+        public IEnumerable<int> Path() {
+            FinalCompute(); // calculates the rest, if needed
 
-
-            return [];
+            return _path;
         }
 
         public DMesh3 Mesh() {
-            FinalCompute();
+            FinalCompute(); // calculates the rest, if needed
 
-            return new();
+            return _mesh;
         }
-
-        // check angle formed by path at each vertex, all angles > pi implies geodesic
-        // flip edges where this is not true
-
-        // flip out subroutine
-        // where path makes angle < pi, flip the edge
-        // if outer angle is less than pi, flip the edge
 
         // stopping threshold to prevent loops from converging
 
--- Fabolus.Core/Meshes/PartingTools/PartingLine.cs
+++ Fabolus.Core/Meshes/PartingTools/PartingLine.cs
@@ -14,7 +14,6 @@ using static MR.DotNet;
 namespace Fabolus.Core.Meshes.PartingTools;
 
 public static partial class PartingTools {
-    private const double DEFAULT_SHARP_ANGLE = MathUtil.Deg2Rad * 30.0;
 
     /// <summary>
     /// Calculates the parting line of a model
@@ -22,8 +21,8 @@ public static partial class PartingTools {
     /// <returns>An ordered array of indexes of the points along the mesh representing the parting line</returns>
     public static Result<Vector3[]> PartingLine(MeshModel model, DraftCollection drafts) {
         // get triangle ids of the negative pull direction region on the mesh
-        
         var region_ids = drafts.GetDraftRegion(DraftClassification.NEGATIVE).ToArray();
+
         var path = model.GetBorderEdgeLoop(region_ids).ToArray(); // a list of vert IDs on the mesh
         Smooth(model, ref path);
 
@@ -31,137 +30,12 @@ public static partial class PartingTools {
     }
 
     private static void Smooth(DMesh3 mesh, ref int[] path) {
-        Graph graph = new(mesh);
-        GeodiscPathing geodiscPathing = new(mesh, path);
 
-        List<int> smoothPath = [];
+        // perform a geodisc pathing check
+        GeodiscPathing geodisc = new(mesh, path);
+        geodisc.Compute();
 
-        int eId0, eId1, eId2;
-        int v0, v1, v2;
-        Index4i e0, e1, e2;
-        int tId;
-        double distance;
-        double length;
-        for (int i = 1; i < path.Length - 1; i++) {
-            // reset
-            tId = -1;
-
-            // simplifying vertex id referencing
-            v0 = path[i - 1];
-            v1 = path[i];
-            v2 = path[i + 1];
-
-            // find the previous edge in the path
-            // abort if cannot find it
-            // TODO: why are we referencing points that have no edges between?
-            // TOD: do this sweep at first to check topology and error, rest of the code would be easier
-            if (!GetEdge(mesh, v1, v0, out e0)) {
-                smoothPath.Add(v1);
-                continue;
-            }
-
-            // find the next edge in the path
-            // abort if cannot find it
-            if (!GetEdge(mesh, v1, v2, out e1)) {
-                smoothPath.Add(v1);
-                continue;
-            }
-
-            // if the edges share a triangle, we can check if the third edge is shorter than the current edges
-            // if so, we can skip saving this point to shorten the parting line
-            if (ThirdTriangleEdgeShorter(mesh, v0, v1, v2)) {
-                continue;
-            }
-
-            // if the angle is sharp, we can check if the other path is shorter
-            if (SharpAngle(mesh, v0, v1, v2, DEFAULT_SHARP_ANGLE) && OtherPathShorter(mesh, graph, v0, v1, v2, out int[] ids)) {
-                // if so, add the new path to the smooth path
-                smoothPath.AddRange(ids);
-                continue;
-            } 
-
-            // if no changes needed, add the current point
-            smoothPath.Add(path[i]);
-        }
-
-        path = smoothPath.ToArray();
+        path = geodisc.Path().ToArray();
     }
 
-    /// <summary>
-    /// If v0, v1, v2 are connected by two triangles, check if the opposing edge vert results in a shorter path
-    /// </summary>
-    /// <param name="ids">array of points for other path</param>
-    /// <returns>If the other point results in a shorter path</returns>
-    private static bool OtherPathShorter(DMesh3 mesh, Graph graph, int vId0, int vId1, int vId2, out int[] ids) {
-        ids = graph.FindPath(vId0, vId2, true);
-
-        if (ids.Length < 2) { return false; } // no path found
-
-        double original_length = FindLength(mesh, vId0, vId1, vId2); // current edges length
-        double graph_length = FindLength(mesh, [vId0, .. ids, vId2]); // new edges length
-        return (original_length > graph_length); // if the new path is shorter, return true
-    }
-
-    private static bool ThirdTriangleEdgeShorter(DMesh3 mesh, int v0, int v1, int v2) {
-        // do edges share a triangle?
-        var tId = mesh.FindTriangle(v0, v1, v2);
-        if ( tId == DMesh3.InvalidID) { return false; } // do not share a triangle
-
-       
-        double distance = DistanceBetweenVectors(mesh, v0, v2); // third edge length
-        double length = FindLength(mesh, v0, v1, v2); // current edges length
-
-        return distance < length;
-    }
-
-    /// <summary>
-    /// Returns true if the angle between v0, v1, v2 is greater than the specified angle in radians.
-    /// </summary>
-    private static bool SharpAngle(DMesh3 mesh, int vId0, int vId1, int vId2, double angle_rads) {
-        Vector3d v0 = mesh.GetVertex(vId0);
-        Vector3d v1 = mesh.GetVertex(vId1);
-        Vector3d v2 = mesh.GetVertex(vId2);
-
-        Vector3d edge1 = (v0 - v1).Normalized;
-        Vector3d edge2 = (v2 - v1).Normalized;
-
-        return edge1.AngleR(edge2) > angle_rads;
-    }
-
-    private static void GeodiscPath(DMesh3 mesh, int vId0, int vId1, int vId2) {
-        if (!SharpAngle(mesh, vId0, vId1, vId2, Math.PI)) {
-            return; // no sharp angle, no need to check
-        }
-
-
-    }
-
-    private static bool GetEdge(DMesh3 mesh, int a, int b, out Index4i edge) {
-        int id = mesh.FindEdge(a, b);
-        if (id < 0) {
-            edge = default;
-            return false; // invalid ID
-        }
-        
-        edge = mesh.GetEdge(id);
-        return true; 
-    }
-
-    private static double FindLength(DMesh3 mesh, params int[] ids) {
-        if (ids.Length < 2) { return 0.0; } // insufficient ids
-
-        double length = 0.0;
-        int current_id = ids[0];
-        int next_id;
-        for (int i = 1; i < ids.Length; i++) {
-            next_id = ids[i];
-            length += DistanceBetweenVectors(mesh, current_id, next_id);
-            current_id = next_id;
-        }
-
-        return length;
-    }
-
-    private static double DistanceBetweenVectors(DMesh3 mesh, int vId0, int vId1) =>
-        mesh.GetVertex(vId0).Distance(mesh.GetVertex(vId1));
 }
