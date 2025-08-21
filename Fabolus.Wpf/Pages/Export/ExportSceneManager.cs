b80135ecf3341c08da5f426d7b163228b9db6022
NathanSmela <nsmela@hotmail.com>
2025-08-21 08:22:29 -0700

    export updated

--- Fabolus.Wpf/Pages/Export/ExportSceneManager.cs
+++ Fabolus.Wpf/Pages/Export/ExportSceneManager.cs
@@ -13,47 +13,61 @@ namespace Fabolus.Wpf.Pages.Export;
 public class ExportSceneManager : SceneManagerBase {
     private BolusModel _bolus;
     private MouldModel _mould;
-
-    private Material _mouldSkin = DiffuseMaterials.Ruby;
+    private bool _showBolus;
+    private bool _showMould;
 
     public ExportSceneManager() {
         RegisterMessages();
+        RegisterInputBindings();
 
-        UpdateDisplay(_bolus);
+        UpdateDisplay();
     }
 
-    protected override void RegisterMessages() {
-        WeakReferenceMessenger.Default.UnregisterAll(this);
+    protected override void RegisterMessages() { 
+        _messenger.Register<ExportSceneManager, ExportShowBolusMessage>(this, (r,m) => {
+            _showBolus = m.ShowBolus;
+            UpdateDisplay();
+        });
 
-        _bolus = WeakReferenceMessenger.Default.Send(new BolusRequestMessage());
-        _mould = WeakReferenceMessenger.Default.Send<MouldRequestMessage>();
+        _messenger.Register<ExportSceneManager, ExportShowMouldMessage>(this, (r, m) => {
+            _showMould = m.ShowMould;
+            UpdateDisplay();
+        });
     }
 
-    void UpdateDisplay(BolusModel? bolus) {
+    void UpdateDisplay() {
+        _bolus = _messenger.Send<BolusRequestMessage>();
+        _mould = _messenger.Send<MouldRequestMessage>();
+
         if (BolusModel.IsNullOrEmpty(_bolus)) {
-            WeakReferenceMessenger.Default.Send(new MeshDisplayUpdatedMessage([]));
+            _messenger.Send(new MeshDisplayUpdatedMessage());
             return;
         }
 
         var models = new List<DisplayModel3D>();
 
+        // show both models unless hovering over a button to export
+        // then show the model about to be exported
+
         //bolus
-        if (_mould.Geometry is null) { 
+        if (!_showMould ) { 
             models.Add(new DisplayModel3D {
                 Geometry = _bolus.Geometry,
                 Transform = MeshHelper.TransformEmpty,
                 Skin = DiffuseMaterials.Gray,
             });
-        } else { 
+
+        } 
         //mould
+        if (!MouldModel.IsNullOrEmpty(_mould) && !_showBolus) { 
             models.Add(new DisplayModel3D {
                 Geometry = _mould.Geometry,
                 Transform = MeshHelper.TransformEmpty,
-                Skin = _mouldSkin,
+                Skin = DiffuseMaterials.Ruby,
                 IsTransparent = true
             });
         }
 
-        WeakReferenceMessenger.Default.Send(new MeshDisplayUpdatedMessage(models));
+        _messenger.Send(new MeshDisplayUpdatedMessage(models));
     }
 }
